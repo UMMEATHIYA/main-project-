83c0d237fce9d6855e8ff0e6aa309914dcf0b293
Umme Athiya <43459908+UMMEATHIYA@users.noreply.github.com>
2021-04-30 16:43:12 +0530

    deleted

--- Project-Management/src/main/java/com/example/projectmanagement/controller/ResourceNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package com.example.projectmanagement.controller;
-
-public class ResourceNotFoundException extends Exception {
-    public ResourceNotFoundException(String s) {
-    }
-}
--- Project-Management/src/main/java/com/example/projectmanagement/controller/Valid.java
+++
@@ -1,4 +0,0 @@
-package com.example.projectmanagement.controller;
-
-public @interface Valid {
-}
